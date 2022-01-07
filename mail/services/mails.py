34e7239b330c79cc5078afe7e04033aa3f28e140
antipups <kurkurin2001@gmail.com>
2022-01-07 17:26:15 +0300

    1) Сделано получение папок; 2) Небольшой рефактор кода; 3) Сделан вывод сообщений по папкам.

--- mail/services/mails.py
+++ mail/services/mails.py
@@ -1,4 +1,4 @@
-from imap_tools import MailBox
+from imap_tools import MailBox, AND
 from mail.services.config import Servers, MaxOutMessages
 
 
@@ -6,10 +6,14 @@ def get_server(email: str) -> str:
     return Servers.Mail if 'mail' in email else Servers.Yandex
 
 
-def get_messages(login: str, password: str) -> list:
+def get_messages(login: str, password: str, folder: str) -> list:
     messages = []
-    with MailBox(get_server('imap.mail.com'))\
+    with MailBox(get_server(login))\
             .login(login, password) as mail:
+
+        if folder:
+            mail.folder.set(folder=folder)
+
         for message in mail.fetch(limit=MaxOutMessages):
             attachs = [{'name': attach.filename, 'content': attach.payload} for attach in message.attachments]
             messages.append({'subject': message.subject,
@@ -20,6 +24,16 @@ def get_messages(login: str, password: str) -> list:
                              'id': int(message.uid)
                              # 'attachments': attachs,
                              })
-    print(messages)
 
-    return messages
+    return messages[::-1]
+
+
+def get_folders(login: str, password: str) -> tuple:
+    with MailBox(get_server(login))\
+            .login(login, password) as mail:
+        return tuple({"id": folder.name,
+                      "title": folder.name} for folder in mail.folder.list())
+
+
+if __name__ == '__main__':
+    ...
--- mail/urls.py
+++ mail/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-
-from mail import views
+from mail import views, controllers
 
 urlpatterns = [
-    path('get_messages', views.get_messages)
+    path('get_messages', controllers.get_messages),
+    path('get_folders', controllers.get_folders)
 ]
 
